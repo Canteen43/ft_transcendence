9b23d01668fe7310918276a50aed065a207963ac
Canteen43 <karl.weihmann@gmail.com>
2025-08-23 13:55:11 +0200

    start adding 3D

--- frontend/src/screens/GameScreen.ts
+++ frontend/src/screens/GameScreen.ts
@@ -1,3 +1,4 @@
+import * as BABYLON from 'babylonjs';
 import { Button } from '../components/Button';
 import { Screen } from '../components/Screen';
 
@@ -24,9 +25,34 @@ export class GameScreen extends Screen {
 		// Middle content container
 		const content = document.createElement('div');
 		content.className =
-			'flex-1 flex flex-col items-center justify-center text-center text-grey';
+			'flex-1 flex flex-col items-center justify-center text-center text-grey gap-3';
 		this.element.appendChild(content);
 
+		// Adding 3D rendering canvas
+		const canvas = document.createElement('canvas');
+		canvas.className = 'w-full h-full';
+		content.appendChild(canvas);
+		const engine = new BABYLON.Engine(canvas, true);
+		const scene = new BABYLON.Scene(engine);
+
+		const camera = new BABYLON.FreeCamera(
+			'camera1',
+			new BABYLON.Vector3(0, 5, -10),
+			scene
+		);
+		camera.setTarget(BABYLON.Vector3.Zero());
+		camera.attachControl(canvas, true);
+
+		const light = new BABYLON.HemisphericLight(
+			'light',
+			new BABYLON.Vector3(0, 1, 0),
+			scene
+		);
+		BABYLON.MeshBuilder.CreateSphere('sphere', { diameter: 2 }, scene);
+
+		engine.runRenderLoop(() => scene.render());
+
+		// Button for testing navigation
 		void new Button(
 			'To TournamentScreen',
 			() => {
