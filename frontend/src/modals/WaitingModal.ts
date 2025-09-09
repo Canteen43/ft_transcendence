dbab8e0b1c5be1a3c877dc2adf96212e6bcd556d
Canteen43 <karl.weihmann@gmail.com>
2025-09-09 10:34:25 +0200

    add waiting modal

---
+++ frontend/src/modals/WaitingModal.ts
@@ -0,0 +1,13 @@
+import { Modal } from '../components/Modal';
+
+export class WaitingModal extends Modal {
+	constructor(parent: HTMLElement) {
+		super(parent);
+
+		this.box.classList.add('flex', 'flex-col', 'items-center', 'justify-center', 'gap-2', 'p-4');
+
+		const message = document.createElement('p');
+		message.textContent = 'Waiting for other player(s) ...';
+		this.box.appendChild(message);
+	}
+}
