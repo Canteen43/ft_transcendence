b678500ecc1f214ebf5d43f8b945024168d0b62f
Canteen43 <karl.weihmann@gmail.com>
2025-09-09 14:19:23 +0200

    add player count to state and connect with game files

--- frontend/src/modals/LocalGameModal.ts
+++ frontend/src/modals/LocalGameModal.ts
@@ -1,13 +1,21 @@
 import { Button } from '../components/Button';
 import { Modal } from '../components/Modal';
 import type { GameOptions } from '../misc/GameOptions';
+import { state } from '../misc/state';
 
 export let gameOptions: GameOptions | null = null;
 
 export class LocalGameModal extends Modal {
 	constructor(parent: HTMLElement) {
 		super(parent);
-		this.box.classList.add('flex', 'flex-col', 'items-center', 'justify-center', 'gap-2', 'p-4');
+		this.box.classList.add(
+			'flex',
+			'flex-col',
+			'items-center',
+			'justify-center',
+			'gap-2',
+			'p-4'
+		);
 		new Button('2 players', () => this._2_players(), this.box);
 		new Button('3 players', () => this._3_players(), this.box);
 		new Button('4 players', () => this._4_players(), this.box);
@@ -19,6 +27,8 @@ export class LocalGameModal extends Modal {
 			playerCount: 2,
 			thisPlayer: 1,
 		};
+		state.playerCount = 2;
+		state.thisPlayer = 1;
 		location.hash = '#game';
 	}
 
@@ -28,6 +38,8 @@ export class LocalGameModal extends Modal {
 			playerCount: 3,
 			thisPlayer: 1,
 		};
+		state.playerCount = 3;
+		state.thisPlayer = 1;
 		location.hash = '#game';
 	}
 
