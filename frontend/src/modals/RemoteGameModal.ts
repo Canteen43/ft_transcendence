6069eb33e30d31353ddaaad862611cfa4d43aa07
Canteen43 <karl.weihmann@gmail.com>
2025-09-10 12:42:13 +0200

    add player number logic and api request to remote game buttons

--- frontend/src/modals/RemoteGameModal.ts
+++ frontend/src/modals/RemoteGameModal.ts
@@ -1,9 +1,10 @@
+import { TournamentSchema } from '../../../shared/schemas/tournament';
+import { UserSchema } from '../../../shared/schemas/user';
 import { Button } from '../components/Button';
 import { Modal } from '../components/Modal';
 import { apiCall } from '../utils/apiCall';
 import { WaitingModal } from './WaitingModal';
 import { WaitingModal1v1 } from './WaitingModal1v1';
-
 
 export class RemoteGameModal extends Modal {
 	constructor(parent: HTMLElement) {
@@ -21,16 +22,40 @@ export class RemoteGameModal extends Modal {
 	}
 
 	private _2_players() {
-		// this.joinGame(2);
+		this.joinGame(2);
 		new WaitingModal1v1(this.box);
 	}
 
 	private _tournament() {
-		// this.joinGame(4);
+		this.joinGame(4);
 		new WaitingModal(this.box);
 	}
 
-	// private joinGame(playerCount: number) {
-	// 	const ret = apiCall('POST', `/users${sessionStorage.getItem('username')}`, null, null);
-	// }
+	private async joinGame(playerCount: number) {
+		// GetUser is a placeholder. The real API will provide a list of players waiting
+		const ret = await apiCall(
+			'GET',
+			`/users/login/${sessionStorage.getItem('username')}`,
+			UserSchema
+		);
+		// if (ret) is a placeholder. The real API will check if enough players are waiting
+		if (ret) {
+			console.info('Placeholder condition met');
+			const body = {
+				creator: '550e8400-e29b-41d4-a716-446655440001',
+				participants: [
+					'550e8400-e29b-41d4-a716-446655440001',
+					'550e8400-e29b-41d4-a716-446655440002',
+				],
+			};
+			const ret2 = await apiCall(
+				'POST',
+				`/tournaments`,
+				TournamentSchema,
+				body
+			);
+			if (ret2) console.info('Tournament created with ID:', ret2.id);
+			else console.error('Failed to create tournament');
+		}
+	}
 }
