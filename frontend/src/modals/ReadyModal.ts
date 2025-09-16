f5141c2cdbb7ad2afb11c0b2e425d641c70042e1
Helene Tharrault <htharrau@student.42berlin.de>
2025-09-16 18:28:12 +0200

    Alias working

--- frontend/src/modals/ReadyModal.ts
+++ frontend/src/modals/ReadyModal.ts
@@ -4,14 +4,6 @@ import { Modal } from './Modal';
 export class ReadyModal extends Modal {
 	constructor(parent: HTMLElement) {
 		super(parent);
-		// this.box.classList.add(
-		// 	'flex',
-		// 	'flex-col',
-		// 	'items-center',
-		// 	'justify-center',
-		// 	'gap-2',
-		// 	'p-4'
-		// );
 		new ReadyButton(this.box);
 	}
 }
