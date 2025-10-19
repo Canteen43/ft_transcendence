cb602e548178822e4d3dc02aa9aa939b3fc2e66a
Helene Tharrault <htharrau@student.42berlin.de>
2025-10-19 21:41:15 +0200

    pb with border of auth component drop down fixed

--- frontend/src/buttons/AuthButton.ts
+++ frontend/src/buttons/AuthButton.ts
@@ -252,7 +252,7 @@ export class AuthComponent {
 			'absolute top-full right-0 mt-1 ' +
 			'bg-white divide-y divide-[var(--color1)] text-gray-800 ' +
 			'opacity-0 pointer-events-none transition-all duration-200 ease-in-out ' +
-			'transform z-10 flex flex-col w-full scale-95';
+			'transform z-10 flex flex-col w-full scale-95 overflow-hidden';
 
 		this.dropdown.addEventListener('mouseenter', this.onEnter);
 		this.dropdown.addEventListener('mouseleave', this.onLeave);
@@ -264,7 +264,8 @@ export class AuthComponent {
 			"font-outfit [font-variation-settings:'wght'_900] text-sm sm:text-base " +
 			'px-3 sm:px-6 py-1.5 sm:py-2 transition-colors ' +
 			'bg-[var(--color1)] text-[var(--color3)] ' +
-			'hover:bg-[var(--color5)]';
+			'hover:bg-[var(--color5)]' +
+			'focus:outline-none focus:ring-inset focus:ring-[var(--color3)]';
 		this.twoFABtn.addEventListener('click', this.handle2FA);
 		this.dropdown.appendChild(this.twoFABtn);
 
@@ -275,7 +276,8 @@ export class AuthComponent {
 			"font-outfit [font-variation-settings:'wght'_900] text-sm sm:text-base " +
 			'px-3 sm:px-6 py-1.5 sm:py-2 transition-colors ' +
 			'bg-[var(--color1)] text-[var(--color3)] ' +
-			'hover:bg-[var(--color5)]';
+			'hover:bg-[var(--color5)]' +
+			'focus:outline-none focus:ring-inset focus:ring-[var(--color3)]';
 		this.logoutBtn.addEventListener('click', this.handleLogout);
 		this.dropdown.appendChild(this.logoutBtn);
 
